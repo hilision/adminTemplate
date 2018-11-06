55e69c0ad8b28e303ca3c3def028a9f96d1a24ab
huy <867839060@qq.com>
2018-11-06 19:55:43 +0800

    Fixed an error calling a function that not exist--SmoothlyMenu()

--- admin/js/main.js
+++ admin/js/main.js
@@ -48,7 +48,7 @@
 		
 	$('#sidebarCollapse').on('click', function () {
 		$("body").toggleClass("mini-navbar");
-		SmoothlyMenu();
+		// SmoothlyMenu();
 	});
 	$(document).on('click', '.header-right-menu .dropdown-menu', function (e) {
 		  e.stopPropagation();
